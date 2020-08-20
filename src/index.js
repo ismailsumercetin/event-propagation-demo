76a27fbb7579f295cda905e16f797753d452e8a4
Sumer <sumer@MacBook-Pro-3.local>
2020-08-20 18:30:48 +0300

    color changes respectively upon click (child elements also change color)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+
 
 const one = document.querySelector("#\\31");
 const two = document.querySelector("#\\32");
@@ -7,52 +7,65 @@ const four = document.querySelector("#\\34");
 const five = document.querySelector("#\\35");
 
 five.addEventListener(
-  "click",
+  "mousedown",
   function () {
-    delay(this);
+    setTimeout(() => {
+      changeBg(this);
+    }, 1500)
+    
   },
   false
 );
 
 four.addEventListener(
-  "click",
+  "mousedown",
   function () {
-    delay(this);
+    setTimeout(() => {
+      changeBg(this);
+    }, 1750)
+    
   },
   false
 );
 
 three.addEventListener(
-  "click",
+  "mousedown",
   function () {
-    delay(this);
+    setTimeout(() => {
+      changeBg(this);
+    }, 2000)
+    
   },
   false
 );
 
 two.addEventListener(
-  "click",
+  "mousedown",
   function () {
-    delay(this);
+    setTimeout(() => {
+      changeBg(this);
+    }, 2250)
+    
   },
   false
 );
 
 one.addEventListener(
-  "click",
+  "mousedown",
   function () {
-    delay(this);
+    setTimeout(() => {
+      changeBg(this);
+    }, 2500)
+    
   },
   false
 );
 
-function delay(t) {
-  const timeout = setTimeout(() => {
-    t.style.backgroundColor = "red";
-
-    console.log(t.id);
-  }, 1000);
+function changeBg(t) {
+  document.querySelector("#\\3"+t.id).style.backgroundColor = "red";
+  
 }
+
 
 // Please make each div clickable programmatically, considering UX
 // 1 - Log the id of the clicked div.
